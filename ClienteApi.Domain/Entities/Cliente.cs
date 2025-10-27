09a491204b0dce18422a55ef81aca2edb9a9afc4
Fernando Ferreira <feernando.dev@gmail.com>
2025-10-27 18:22:04 -0300

    criação de entidades

---
+++ ClienteApi.Domain/Entities/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace ClienteApi.Domain.Entities
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public DateTime DataCadastro { get; set; }
+        
+        public Endereco? Endereco { get; set; }
+        public ICollection<Contato> Contatos { get; set; } = new List<Contato>();
+    }
+}
---
+++ ClienteApi.Domain/Entities/Contato.cs
@@ -0,0 +1,11 @@
+﻿namespace ClienteApi.Domain.Entities
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; } = string.Empty;
+        public string Texto { get; set; } = string.Empty;        
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; } = null!;
+    }
+}
